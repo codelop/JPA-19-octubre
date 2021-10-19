c5c3532ebb06d1b11ff8bc224e61d965ec943c13
Jose Antonio ortega <developer@mailmy.com>
2021-10-19 12:09:11 +0200

    Version inicial del projecto JPA

---
+++ src/main/java/com/ortega/yepes/jpa/JPA_19_octubre/App.java
@@ -0,0 +1,13 @@
+package com.ortega.yepes.jpa.JPA_19_octubre;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
